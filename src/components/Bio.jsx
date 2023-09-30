223e3b9a0441035598542e6904b77be256b16f71
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2023-09-30 12:33:32 -0400

    face change

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,7 +4,7 @@ const Bio = () => {
     return (
         <div className="flex justify-center items-center mt-4"> 
             <div className="bio relative w-64 h-96 overflow-hidden">
-                <img src="src/assets/_DSC8108.JPG" alt="Profile Picture" className="w-full h-full object-cover" />
+                <img src="src/assets/svg/face.JPG" alt="Profile Picture" className="w-full h-full object-cover" />
                 <div className="bio-text absolute bottom-4 right-11 text-white">
                     <h1 className="text-xl font-bold">n i c o r o c</h1>
                     <p className="mt-2">r a p p e r <hr /> n y c</p>
